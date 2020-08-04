42e7d27c2f328478d7f23482a7ea63bf0f0926e6
Felipe Souza <fnasci@gmail.com>
2020-08-03 22:26:33 -0300

    Adding pagination

--- public/scripts.js
+++ public/scripts.js
@@ -7,7 +7,8 @@ for (item of menuItems){
     }
 }
 
-function pagination(selectedPage, totalPages){
+
+function paginate(selectedPage, totalPages){
     let pages = [],
         oldPage
 
@@ -31,5 +32,38 @@ function pagination(selectedPage, totalPages){
                 oldPage = currentPage
             }
         }
+        return pages
+    }
+    
+
+function createPagination(){
+
+    const filter = pagination.dataset.filter
+    const page = +pagination.dataset.page
+    const total = +pagination.dataset.total
+    const pages = paginate(page, total)
+        
+    let elements=""
+    
+    for (let page of pages){
+        if(String(page).includes("...")){
+            elements += `<span>${page}</span>`
+    
+        }else{
+            if( filter ){
+                elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+    
+            }else{
+                elements += `<a href="?page=${page}">${page}</a>`
+            }
+        }
+    }
+    
+    pagination.innerHTML = elements
 }
 
+const pagination = document.querySelector(".pagination")
+
+if(pagination){
+    createPagination(pagination)
+}
